f24f61cc11ff57091168496aed673abeb984866c
parkjaewon <wodnjs3336@naver.com>
2024-01-07 19:23:53 +0900

    add icon image

--- app/src/main/java/com/example/applemarket/MainActivity.kt
+++ app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.applemarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val productList = Product.productData()
+
+        val adapter = Adapter(productList)
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+
     }
 }
